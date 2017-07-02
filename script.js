1818dbc97c9bff31e06cda941d89352cc89424b7
cruzsbrian <cruz.s.brian@gmail.com>
2017-07-02 16:05:59 -0400

    Switch to amcharts for graph

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 var ctx;
 var cheight;
 var cwidth;
+var chart;
+var pts;
 
 function main() {
-    ctx = document.getElementById('canvas').getContext('2d');
-    ctx.canvas.width = window.innerWidth;
-    ctx.canvas.height = window.innerHeight * 0.75;
-    cwidth = window.innerWidth;
-    cheight = window.innerHeight * 0.75;
+    // ctx = document.getElementById('canvas').getContext('2d');
+    // ctx.canvas.width = window.innerWidth;
+    // ctx.canvas.height = window.innerHeight * 0.75;
+    // cwidth = window.innerWidth;
+    // cheight = window.innerHeight * 0.75;
     
     sim();
+	makeGraph();
 }
 
 function sim() {
@@ -36,14 +39,14 @@ function sim() {
     var dx = 0;
     
     // pid stuff
-    var setpoint = isNaN(setpointinput) ? 0 : setpointinput;
+    var setpoint = isNaN(setpointinput) ? 2 : setpointinput;
     var kp = isNaN(kpinput) ? 0 : kpinput;
     var ki = isNaN(kiinput) ? 0 : kiinput;
     var kd = isNaN(kdinput) ? 0 : kdinput;
     var errorAccum = 0;
     var lastError = setpoint - x;
     
-    var pts = [];
+    pts = [];
     var volts = [];
     var amps = [];
     for (var t = 0; t < totalTime / timestep; t += 1) {
@@ -76,9 +79,12 @@ function sim() {
         var torque = mass * radius * (ddx + g);
         amps[t] = torque / kt;
         
-        pts[t] = x;
-		
-		//console.log(error);
+        pts[t] = {
+			"t": (t * timestep).toFixed(2),
+			"x": x.toFixed(3),
+			"setpoint": setpoint,
+			"voltage": voltage
+		};
     }
     
     var maxheight = 4;
@@ -92,26 +98,101 @@ function sim() {
     }
 	
 	//clear graph
-	ctx.clearRect(0, 0, cwidth, cheight);
+	//ctx.clearRect(0, 0, cwidth, cheight);
 	
 	// draw setpoint
-    ctx.beginPath();
-    ctx.strokeStyle = 'blue';
-    var height = setpoint / maxheight * cheight * 0.75;
-    ctx.moveTo(0, cheight * 0.75 - height);
-    ctx.lineTo(cwidth, cheight * 0.75 - height);
-    ctx.stroke();
+    // ctx.beginPath();
+    // ctx.strokeStyle = 'blue';
+    // var height = setpoint / maxheight * cheight * 0.75;
+    // ctx.moveTo(0, cheight * 0.75 - height);
+    // ctx.lineTo(cwidth, cheight * 0.75 - height);
+    // ctx.stroke();
     
 	// draw path
-    ctx.beginPath();
-    ctx.strokeStyle = 'black';
-    ctx.moveTo(0, cheight);
-    for (var t = 0; t < totalTime / timestep; t += 1) {
-        var height = pts[t] / maxheight * cheight * 0.75;
-        ctx.lineTo(t * cwidth * timestep / totalTime, cheight * 0.75 - height);
+    // ctx.beginPath();
+    // ctx.strokeStyle = 'black';
+    // ctx.moveTo(0, cheight);
+    // for (var t = 0; t < totalTime / timestep; t += 1) {
+        // var height = pts[t] / maxheight * cheight * 0.75;
+        // ctx.lineTo(t * cwidth * timestep / totalTime, cheight * 0.75 - height);
         
         //console.log(amps[t]);
-    }
-    ctx.stroke();
+    // }
+    // ctx.stroke();
+	if (chart) {
+		chart.dataProvider = pts;
+		chart.validateData();
+	}
+}
+
+function makeGraph() {
+	chart = new AmCharts.AmSerialChart();
+
+	chart.dataProvider = pts;
+	chart.marginLeft = 10;
+	chart.categoryField = "t";
+	//chart.synchronizeGrid = true;
+
+	// AXES
+
+	// value
+	var voltageAxis = new AmCharts.ValueAxis();
+	voltageAxis.id = "voltageAxis";
+	voltageAxis.minimum = -15;
+	voltageAxis.maximum = 15;
+	voltageAxis.position = "right";
+	chart.addValueAxis(voltageAxis);
 	
+	var posAxis = new AmCharts.ValueAxis();
+	posAxis.id = "posAxis";
+	posAxis.maximum = 2.5;
+	posAxis.synchronizeWithAxis(voltageAxis);
+	chart.addValueAxis(posAxis);
+
+	// GRAPH
+	
+	var graphVoltage = new AmCharts.AmGraph();
+	graphVoltage.title = "Voltage";
+	graphVoltage.valueAxis = "voltageAxis";
+	graphVoltage.type = "line";
+	graphVoltage.lineColor = "#ff7777";
+	graphVoltage.lineThickness = 2;
+	graphVoltage.valueField = "voltage";
+	chart.addGraph(graphVoltage);
+	chart.hideGraph(graphVoltage);
+	
+	var graphPos = new AmCharts.AmGraph();
+	graphPos.title = "Position";
+	graphPos.valueAxis = "posAxis";
+	graphPos.type = "line"; // this line makes the graph line.
+	graphPos.lineColor = "#000000";
+	graphPos.lineThickness = 2;
+	graphPos.valueField = "x";
+	chart.addGraph(graphPos);
+	
+	var graphSetpoint = new AmCharts.AmGraph();
+	graphSetpoint.title = "Setpoint";
+	graphSetpoint.valueAxis = "posAxis";
+	graphSetpoint.type = "line"; // this line makes the graph line.
+	graphSetpoint.lineColor = "blue";
+	graphSetpoint.lineThickness = 1;
+	graphSetpoint.valueField = "setpoint";
+	chart.addGraph(graphSetpoint);
+	
+	legend = new AmCharts.AmLegend();
+	legend.align = "left";
+	legend.markerType = "line";
+	legend.markerBorderThickness = 3;
+	legend.valueText = "";
+	chart.addLegend(legend);
+	
+	// CURSOR
+	var chartCursor = new AmCharts.ChartCursor();
+	chartCursor.cursorPosition = "mouse";
+	//chart.addChartCursor(chartCursor);
+
+	chart.creditsPosition = "bottom-right";
+
+	// WRITE
+	chart.write("chartdiv");
 }
